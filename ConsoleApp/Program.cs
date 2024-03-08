c505f92aaeaa0852af7a9f15b89afb4c19d64f03
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2024-03-08 14:57:45 +0100

    Added avg

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello,, World PL!!");;
+Console.WriteLine(GetAvg([1, 2, 3, 4, 5]));
+
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+
+    foreach (int i in arr)
+    {
+        sum += i;
+    }
+
+    return (double) sum / arr.Length;
+}
